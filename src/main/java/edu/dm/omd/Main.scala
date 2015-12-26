e9fc4c79b680713b957b7dbe6bafd2336356c5f1
Arnold <szederjesiarnold@yahoo.com>
2015-12-26 20:38:13 +0200

    Tried tt4j

--- src/main/java/edu/dm/omd/Main.scala
+++ src/main/java/edu/dm/omd/Main.scala
@@ -1,11 +1,25 @@
 package edu.dm.omd
 
-/**
-  * Created by User on 26-12-15.
-  */
-class Main {
+import edu.dm.omd.util.PropertiesUtil
+import org.annolab.tt4j.{TokenHandler, TreeTaggerWrapper}
+
+object Main {
+
   def main(args: Array[String]) {
-    val line = scala.io.StdIn.readLine();
-    
+    PropertiesUtil.loadSystemProperties()
+    val line = scala.io.StdIn.readLine()
+    val tt = new TreeTaggerWrapper[String]()
+    try {
+      tt.setModel("/home/arnold/Programs/DM/treetagger/models/english-utf8.par")
+      tt setHandler new TokenHandler[String]() {
+        def token(token: String, pos: String, lemma: String) {
+          System.out.println(token + "\t" + pos + "\t" + lemma)
+        }
+      }
+      tt.process(line.split(" "));
+    }
+    finally {
+      tt.destroy()
+    }
   }
 }
---
+++ src/main/java/edu/dm/omd/util/PropertiesUtil.scala
@@ -0,0 +1,21 @@
+package edu.dm.omd.util
+
+import java.util.Properties
+import collection.JavaConversions._
+
+import edu.dm.omd.Main
+
+object PropertiesUtil {
+
+  def loadSystemProperties(): Unit = {
+    val properties = new Properties()
+    val inputStream = Main.getClass.getResourceAsStream("system.properties")
+    properties.load(inputStream)
+    for(entry <- properties.entrySet()) {
+      val key = entry.getKey.toString
+      val value = entry.getValue.toString
+      System.setProperty(key, value)
+    }
+  }
+
+}
