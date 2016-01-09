81f53f0b5541071c7d5b01cee6e746c6e8de862d
OnlineTests <greenfleeg@yahoo.com>
2016-01-09 13:24:20 +0200

    Save words to proper file

--- src/main/java/edu/dm/omd/repository/Repository.java
+++ src/main/java/edu/dm/omd/repository/Repository.java
@@ -10,12 +10,12 @@ public enum Repository {
 
     INSTANCE;
 
-    private final String positiveListFileName = PropertiesUtil.getProperty("positiveListFilePath");
-    private final String negativeListFileName = PropertiesUtil.getProperty("negativeListFilePath");
-    private final String neutralListFileName = PropertiesUtil.getProperty("neutralListFilePath");
+    private final String positiveListFilePath = PropertiesUtil.getProperty("positiveListFilePath");
+    private final String negativeListFilePath = PropertiesUtil.getProperty("negativeListFilePath");
+    private final String neutralListFilePath = PropertiesUtil.getProperty("neutralListFilePath");
 
-    private final Set<String> positiveWords = FilesUtil.readFromFile(positiveListFileName);
-    private final Set<String> negativeWords = FilesUtil.readFromFile(negativeListFileName);
+    private final Set<String> positiveWords = FilesUtil.readFromFile(positiveListFilePath);
+    private final Set<String> negativeWords = FilesUtil.readFromFile(negativeListFilePath);
     private final Set<String> neutralWords = new HashSet<>();
     private final Set<String> newPositiveWords = new HashSet<>();
     private final Set<String> newNegativeWords = new HashSet<>();
@@ -39,4 +39,10 @@ public enum Repository {
     public void addNeutralWord(String word) {
         neutralWords.add(word);
      }
+
+    public void addWordsToFile() {
+        FilesUtil.appendToFile(positiveListFilePath, newPositiveWords);
+        FilesUtil.appendToFile(negativeListFilePath, newNegativeWords);
+        FilesUtil.writeToFile(neutralListFilePath, neutralWords);
+    }
 }
